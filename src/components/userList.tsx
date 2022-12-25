83d7b13ab41117eb34166003a9d0e3e93785fd19
main <you@example.com>
2022-12-25 14:53:34 +0600

    мердж

--- src/components/userList.tsx
+++ src/components/userList.tsx
@@ -1,33 +1,31 @@
-import React, {useEffect} from 'react';
-import {useDispatch} from "react-redux";
-import {useTypeSelector} from "../hooks/useTypeSelector";
-import {fetchUsers} from "../store/action-creators/user";
+import React, { useEffect } from 'react';
+import { useDispatch } from "react-redux";
+import { useTypeSelector } from "../hooks/useTypeSelector";
+import { fetchUsers } from "../store/action-creators/user";
 
+const UserList: React.FC = () => {
+    const { users, error, loading } = useTypeSelector(state => state.user)
 
-
-
-
-
-
-
-const UserList : React.FC= () => {
-    const {users,error,loading}= useTypeSelector(state => state.user)
-
-    const dispatch=useDispatch()
+    const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(fetchUsers())
     }, [])
 
-    if(loading){
+
+
+
+    if (loading) {
         return <h1>Идет загрузка ...</h1>
     }
-    if(error){
+    if (error) {
         <h1>{error}</h1>
     }
     return (
+
+
         <div>
-            {users.map(user=> <div key={user.id}>{user.name}</div>)}
+            {users.map(user => <div key={user.id}>{user.name}</div>)}
 
         </div>
     );
